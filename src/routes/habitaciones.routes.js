1c0fea7b611b88c73a56046e8b20b617d582ecff
ismael <joseismaelvargas@users.noreply.github.com>
2025-03-30 16:21:24 -0300

    priv

--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { crearHabitacion } from "../controllers/Habitaciones.js";
+import { prueba } from "../controllers/Habitaciones.js";
 const router=Router();
 router.route('/crear').post(crearHabitacion)
+router.route('/').get(prueba)
 
 export default router 
